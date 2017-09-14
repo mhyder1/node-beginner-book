41f10d84bda64d94ef0683d688ff39672b8ba2d8
Muhammad Abdusamad <muhammadabdusamad@yahoo.com>
2017-09-13 21:44:16 -0500

    added router

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 'use strict';
 
 var http = require('http');
+var url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(req, res) {
-        console.log('Request received');
+        var pathname = url.parse(req.url).pathname;
+        console.log(`Request for ${pathname} received.`);
+        
+        route(pathname);
+        
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.write('Hello World');
         res.end();
     }
     http.createServer(onRequest).listen(8888);
+    console.warn('Server started on port 8888');
 }
 
 
